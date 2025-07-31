73575e660c46a7d536ae6385cba66c32d7646aec
kamvalethuzitho <zithokamvalethu@gmail.com>
2025-07-31 17:27:39 +0200

    removed console.log's

--- was_package_received_yesterday.js
+++ was_package_received_yesterday.js
@@ -8,13 +8,3 @@ function was_package_received_yesterday(tz_from,tz_to, start,duration)
     const localReceive = utcReceived + tz_to;
     return localReceive < 0 ;
 }
-
-
-    console.log(was_package_received_yesterday(1,4,8,8)) // false
-    console.log(was_package_received_yesterday(10, -1, 1, 2)); // true
-
-
-console.log(was_package_received_yesterday(3, 0, 13, 1)); // false
-console.log(was_package_received_yesterday(12, -3, 5, 8)); // true
-console.log(was_package_received_yesterday(0, 0, 0, 0)); // false
-console.log(was_package_received_yesterday(10, -11, 1, 2)); // true
