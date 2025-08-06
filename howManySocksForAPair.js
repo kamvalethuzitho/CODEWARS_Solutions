e95510470a9a59b3a6be5c81b19ca9e184073e36
kamvalethuzitho <zithokamvalethu@gmail.com>
2025-08-06 16:20:03 +0200

    Created a kata that determines the minimum number of socks to draw to guarantee a given number of matching pairs, regardless of sock colours.

---
+++ howManySocksForAPair.js
@@ -0,0 +1,8 @@
+function socks(colours,pairs){
+return pairs * 2 + (colours - 1);
+}
+console.log(socks(2, 1));
+console.log(socks(1, 2));
+console.log(socks(2, 3));
+console.log(socks(4, 2)); 
+console.log(socks(4, 4)); 
